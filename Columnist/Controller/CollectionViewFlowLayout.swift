f5176edf0dc29aa5c646694f838b9d0f5d90eae5
ShootingStar <674274407@qq.com>
2016-07-14 18:41:39 +0800

    流水布局
    还没完成

---
+++ Columnist/Controller/CollectionViewFlowLayout.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewFlowLayout.swift
+//  HuaTianXiaoQi
+//
+//  Created by lc－mac on 16/7/14.
+//  Copyright © 2016年 lc－mac. All rights reserved.
+//
+
+import Foundation
+
+class CollectionViewFlowLayout:  {
+    <#code#>
+}
---
+++ Columnist/Controller/LevitateHeaderFlowLayout.swift
@@ -0,0 +1,24 @@
+//
+//  LevitateHeaderFlowLayout.swift
+//  HuaTianXiaoQi
+//
+//  Created by lc－mac on 16/7/14.
+//  Copyright © 2016年 lc－mac. All rights reserved.
+//
+
+import UIKit
+
+/// 可以让 Header 悬浮的流布局
+
+//这个弄成自适应流布局最好 但它这个不是流布局
+class LevitateHeaderFlowLayout: UICollectionViewFlowLayout {
+    
+    //重写父类的更新方法
+    override func prepareLayout() {
+        super.prepareLayout()
+        
+        
+        
+        
+    }
+}
